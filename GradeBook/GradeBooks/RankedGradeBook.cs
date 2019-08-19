9478ec5d6cdea363de1150447ae19cd3583d843d
Shabar Mohajer-Iravani <shabar.mohaje@microsoft.com>
2019-08-19 15:05:01 -0400

    Ranked LetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,15 +1,47 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-	public class RankedGradeBook:BaseGradeBook	
+	public class RankedGradeBook : BaseGradeBook
 	{
-		public RankedGradeBook(string name):base(name)
+		public RankedGradeBook(string name) : base(name)
 		{
 			Type = GradeBookType.Ranked;
+		}
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count < 5)
+			{
+				throw new InvalidOperationException("Ranked grading requires a minimum of 5 students to work.");
+			}
+
+			var threshold = (int)Math.Ceiling((Students.Count * 0.2));
+			var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+			if (grades[threshold - 1] <= averageGrade)
+			{
+				return 'A';
+			}
+			else if (grades[(threshold * 2) - 1] <= averageGrade)
+			{
+				return 'B';
+			}
+			else if (grades[(threshold * 3) - 1] <= averageGrade)
+			{
+				return 'C';
+			}
+			else if (grades[(threshold * 4) - 1] <= averageGrade)
+			{
+				return 'D';
+			}
+			else
+			{
+				return 'F';
+			}
 		}
 	}
 }
